7fa9f859e93b4596ad6aba2ea946c9bcac1e5764
Ben Masters <bmasters8@gatech.edu>
2018-12-14 15:36:33 -0800

    validation implemented along with warnings

--- BodyWeightTracker/View Controllers/ViewController.swift
+++ BodyWeightTracker/View Controllers/ViewController.swift
@@ -28,6 +28,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var backgroundContainer: UIView!
     
+    @IBOutlet weak var bfWarning: UILabel!
+    
+    @IBOutlet weak var wtWarning: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,18 +69,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
         
         
-        
-        
     }
     
     
-    func themeToggle() {
+    private func themeToggle() {
         if toggleValue.isOn {
             backgroundContainer.backgroundColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
         }
         else {
             backgroundContainer.backgroundColor = #colorLiteral(red: 0.1411764771, green: 0.3960784376, blue: 0.5647059083, alpha: 1)
         }
+    }
+    
+    private func displayWarning(textfield: UITextField, containsNonDigitChars: Bool) {
+        if containsNonDigitChars != true {
+            if textfield == bodyFatField {
+                bfWarning.textColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1).withAlphaComponent(0.0)
+            }
+            else if textfield == weightField {
+                wtWarning.textColor =  #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1).withAlphaComponent(0.0)
+            }
+            
+        }
+        else {
+            if textfield == bodyFatField {
+                bfWarning.textColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1).withAlphaComponent(1.0)
+            }
+            else if textfield == weightField {
+                wtWarning.textColor =  #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1).withAlphaComponent(1.0)
+            }
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let invalidCharacters = CharacterSet(charactersIn: "0123456789.").inverted
+        let test = string.rangeOfCharacter(from: invalidCharacters)
+        let check = test == nil
+        displayWarning(textfield: textField, containsNonDigitChars: !check)
+        return check
     }
     
     private func getNoon()-> Date {
@@ -106,18 +136,16 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let context = container.viewContext
         
         if bf.count > 1 && wt.count > 1 {
-            
-            let date = Date()
             var calendar = Calendar.current
             calendar.timeZone = NSTimeZone.local
             
             // Get today's beginning & end
             let dateFrom = calendar.startOfDay(for: Date())
-            let dateTo = calendar.date(byAdding: .day, value: 1, to: dateFrom)
+            let dateTo = calendar.date(byAdding: .day, value: 1, to: dateFrom)!
             
             // Set predicate as date being today's date
-            let fromPredicate = NSPredicate(format: "%@ >= %@", date as NSDate, dateFrom as NSDate)
-            let toPredicate = NSPredicate(format: "%@ < %@", date as NSDate, dateTo! as NSDate)
+            let fromPredicate = NSPredicate(format: "dateCreated >= %@", dateFrom as NSDate)
+            let toPredicate = NSPredicate(format: "dateCreated < %@", dateTo as NSDate)
             let datePredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [fromPredicate, toPredicate])
             
             let request: NSFetchRequest<MeasurementEntry> = MeasurementEntry.fetchRequest()
@@ -141,7 +169,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
                 
             }
             else {
-            
+                
                 let entry = MeasurementEntry(context: context)
                 entry.dateCreated = Date()
                 let beforeNoon = (entry.dateCreated! < getNoon())
